5f5b5bbfe944aef5b6fd5fc9302827c87ebe8728
Pikkel Rick <larsvandijk@hotmail.com>
2023-11-07 00:08:27 +0100

    Created LineChart.svelte

---
+++ src/LineChart.svelte
@@ -0,0 +1,15 @@
+<script>
+    import * as d3 from "d3";
+
+    export let data = {};
+
+    const width = 800;
+    const height = 600;
+
+    const xScale = d3.scaleLinear()
+
+    const yScale = d3.scaleLinear(
+    [0, d3.max(data, (d) => d.close)],
+    [height - marginBottom, marginTop]
+  );
+</script>
